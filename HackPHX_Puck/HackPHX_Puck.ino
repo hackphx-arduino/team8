70cc6c59af16a8e031a564c419d9402071b50fe9
Matthew Shepard <mattshepard@gmail.com>
2013-03-02 16:06:33 -0700

    Puck

--- HackPHX_Puck/HackPHX_Puck.ino
+++ HackPHX_Puck/HackPHX_Puck.ino
@@ -1,5 +1,4 @@
-#include <AccelStepper.h>
-#include <AFMotor.h>
+#include <Shieldbot.h>
 
 #define COMMAND_KEY 33
 #define CMD_RETRACT 65
@@ -8,114 +7,50 @@
 
 #define STATUS_LED_PIN 13
 
+#define MOTOR_PWR 96
+
 
 byte msg[2];
+
+Shieldbot shieldbot = Shieldbot();
 
 void setup()
 {
 	Serial.begin(9600);
         pinMode(13,OUTPUT);
         delay(500);
-        nextReadyX = millis()+1000;
-        nextReadyY = millis()+1000;
-        xAxis.setMaxSpeed(STEP_PER_ROTATION_X);
-        yAxis.setMaxSpeed(STEP_PER_ROTATION_Y);
-        xAxis.setAcceleration(STEP_PER_ROTATION_X);
-        yAxis.setAcceleration(STEP_PER_ROTATION_Y);
-        xAxis.setCurrentPosition(0);
-        yAxis.setCurrentPosition(0);
+        shieldbot.setMaxSpeed(255);
 }
 
 void loop()
 {
 
-  if (Serial.available() > 3) {
+  if (Serial.available() > 1) {
     while(Serial.peek() != COMMAND_KEY){
       Serial.read();//Discard mis-aligned data.
     }
-    int len = Serial.readBytes((char*)msg, 4);
-    if (len == 4 && msg[0]==COMMAND_KEY) {
+    int len = Serial.readBytes((char*)msg, 2);
+    if (len == 2 && msg[0]==COMMAND_KEY) {
       switch(msg[1]){
-        case CMD_DRIVE_X_FWD:
-          Serial.print("X-FWD-");
-          length = (((uint16_t) msg[2])<<8) | msg[3];
-          Serial.println(length);
-          xAxis.move(length);
-          xRunning = true;
+        case CMD_RETRACT:
+          Serial.println("RET");
+          shieldbot.drive(-MOTOR_PWR,-MOTOR_PWR);
+          delay(500);
+          shieldbot.drive(0,0);
         break;
-        case CMD_DRIVE_X_BACK:
-          Serial.print("X-BACK-");
-          length = (((uint16_t) msg[2])<<8) | msg[3];
-          Serial.println(length);
-          xAxis.move(-length);
-          xRunning = true;
+        case CMD_EXTEND:
+          Serial.println("EXT");
+          shieldbot.drive(MOTOR_PWR,MOTOR_PWR);
+          delay(500);
+          shieldbot.drive(0,0);
         break;
-        case CMD_DRIVE_Y_FWD:
-          Serial.print("Y-FWD-");
-          length = (((uint16_t) msg[2])<<8) | msg[3];
-          Serial.println(length);
-          yAxis.move(length);
-          yRunning = true;
-        break;
-        case CMD_DRIVE_Y_BACK:
-          Serial.print("Y-BACK-");
-          length = (((uint16_t) msg[2])<<8) | msg[3];
-          Serial.println(length);
-          xAxis.move(-1*length);
-          yRunning = true;
-        break;
-        case CMD_DRIVE_X_SPS:
-          Serial.print("X-SPS-");
-          Serial.println(msg[2]);
-          xAxis.setMaxSpeed(msg[2]);
-        break;
-        case CMD_DRIVE_Y_SPS:
-          Serial.print("Y-SPS-");
-          Serial.println(msg[2]);
-          yAxis.setMaxSpeed(msg[2]);
-        break;
-        case CMD_DRIVE_STEPMODE:
-          switch(msg[2]){
-            case STEPMODE_SINGLE:
-              stepperMode = SINGLE;
-              break;
-            case STEPMODE_DOUBLE:
-              stepperMode = DOUBLE;
-              break;
-            case STEPMODE_INTERLEAVE:
-              stepperMode = INTERLEAVE;
-              break;
-            case STEPMODE_MICROSTEP:
-              stepperMode = MICROSTEP;
-              break;
-          }
-          Serial.write(currentStatus);
+        case CMD_REST:
+          Serial.print("REST");
+          shieldbot.drive(0,0);
         break;
       }
     }
   }
-  if(xRunning){
-    xAxis.run();
-    if(xAxis.distanceToGo() == 0){
-      Serial.write(STATUS_X_READY);
-      xRunning = false;
-    }
-  }else{
-    if(nextReadyX < millis()){
-      Serial.write(STATUS_X_READY);
-      nextReadyX = millis() + 5000;
-    }
-  }
-  if(yRunning){
-    yAxis.run();
-    if(yAxis.distanceToGo() == 0){
-      Serial.write(STATUS_Y_READY);
-      yRunning = false;
-    }
-  }else{
-    if(nextReadyY < millis()){
-      Serial.write(STATUS_Y_READY);
-      nextReadyY = millis() + 5000;
-    }
-  }
+  
+  
 }
